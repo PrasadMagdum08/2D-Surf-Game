4f0e5ebf4e3537d228e03d7e561575e269c2db4d
Prasad Magdum <153984996+PrasadMagdum08@users.noreply.github.com>
2024-09-01 19:05:55 +0000

    add

--- Surfer.py
+++ Surfer.py
@@ -2,7 +2,7 @@ from settings import *
 
 class Surfer:
     def __init__(self):
-        self.image = pygame.image.load("icons/surfer.svg").convert_alpha()
+        self.image = pygame.image.load("items/surfer.svg").convert_alpha()
         self.image = pygame.transform.scale(self.image, (70, 70))
         self.rect = self.image.get_rect()
         self.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 100)
