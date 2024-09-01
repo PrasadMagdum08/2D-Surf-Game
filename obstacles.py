73b841e16ec577c30488143c03c842d4e84c2595
Prasad Magdum <153984996+PrasadMagdum08@users.noreply.github.com>
2024-09-01 18:02:19 +0000

    add

---
+++ obstacles.py
@@ -0,0 +1,35 @@
+from settings import *
+import random
+
+class Wave:
+    def __init__(self):
+        self.image = pygame.image.load("icons/waves.png").convert_alpha()
+        self.surf = pygame.Surface((SCREEN_WIDTH, 10))
+        # self.image.fill(BLUE)
+        self.rect = self.surf.get_rect()
+        self.rect.y = random.randint(-SCREEN_HEIGHT, 0)
+
+    def move(self):
+        self.rect.y += random.randint(0, 5)
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.y = random.randint(-SCREEN_HEIGHT, 0)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+class Obstacle:
+    def __init__(self):
+        self.image = pygame.image.load("obstacles/rock1.jpg").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, max(0, max(0, SCREEN_WIDTH - self.rect.width)))
+        self.rect.y = random.randint(-SCREEN_HEIGHT, 0)
+
+    def move(self):
+        self.rect.y += 5
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.y = random.randint(-SCREEN_HEIGHT, 0)
+            self.rect.x = random.randint(0, max(0, SCREEN_WIDTH - self.rect.width))
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
---
+++ settings.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+# Initialize Pygame
+pygame.init()
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Colors
+# WHITE = (255, 255, 255)
+# BLUE = (0, 0, 255)
+# GREEN = (0, 255, 0)
+# RED = (255, 0, 0)
+
+# Create the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("2D Surf Game")
+
+# Clock for controlling the frame rate
+clock = pygame.time.Clock()
